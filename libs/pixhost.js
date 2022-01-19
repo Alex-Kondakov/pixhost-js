c0618f7ccbd7d65c8498fc388816f71f89d27843
Aleksandr Kondakov <deadtototo@gmail.com>
2022-01-19 13:27:10 +0400

    double uploadImage remove

--- libs/pixhost.js
+++ libs/pixhost.js
@@ -31,30 +31,6 @@ exports.init = () => {
                 })
             })
         },
-                //Upload image. filePath - path to image. content_type - adult content, 1 if true, 0 otherwise. max_th_size - maximal thumbnail size - integer between 150 and 500. gallery_hash, gallery_upload_hash - for upload to gallery
-        uploadImage (filePath, content_type=1, max_th_size=320, gallery_hash, gallery_upload_hash) {
-            let fileStream = fs.createReadStream(filePath);
-            let data = {
-                img: fileStream,
-                content_type: content_type,
-                max_th_size: max_th_size
-            }
-            if (gallery_hash) {
-                data.gallery_hash = gallery_hash;
-            }
-            if (gallery_upload_hash) {
-                data.gallery_upload_hash = gallery_upload_hash;
-            }
-            return new Promise ((resolve, reject) => {
-                request.post({url: 'https://api.pixhost.to/images', headers: {'Content-Type': 'multipart/form-data; charset=utf-8', 'Accept': 'application/json'}, formData: data}, (err, httpResponse, body) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(JSON.parse(body));
-                    }
-                })
-            })
-        },
         //Upload cover. PixHost.to cover is two images joined horizontally. images - array of left and right images (paths to each). right image is optional. content_type - adult content, 1 if true, 0 otherwise. gallery_hash, gallery_upload_hash - for upload to gallery
         uploadCover (images, content_type=1, gallery_hash, gallery_upload_hash) {
             let img_left = fs.createReadStream(images[0]);
